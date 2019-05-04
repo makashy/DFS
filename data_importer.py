5065352e0ae97a4e93e004df437f29c80076a12b
Amin <m.amin.kashi@gmail.com>
2019-05-04 22:15:04 +0430

    Update data_importer.py
    class DatasetGenerator added
    class  SynthiaSf updated
    class VIPER added

--- data_importer.py
+++ data_importer.py
@@ -3,10 +3,157 @@
 from __future__ import absolute_import, division, print_function
 
 import os
-import pandas as pd
+
 import numpy as np
+import pandas as pd
 import tables
-import imageio
+from skimage import img_as_float32
+from skimage import img_as_float64
+from skimage.io import imread
+from skimage.transform import resize
+from skimage import img_as_ubyte
+
+import segmentation_dics
+
+
+class DatasetGenerator:
+    """Abstract iterator for looping over elements of a dataset ."""
+
+    def __init__(self,
+                 dataset_dir,
+                 batch_size=1,
+                 repeater=False,
+                 shuffle=False,
+                 output_shape=None,
+                 data_type='float64',
+                 label_type=('segmentation', 'instance', 'depth')):
+        self.dataset_dir = dataset_dir
+        self.batch_size = batch_size
+        self.repeater = repeater
+        self.shuffle = shuffle
+        self.output_shape = output_shape
+        self.data_type = data_type
+        self.label_type = label_type
+        self.dataset = self.data_frame_creator()
+        self.index = self.dataset.shape[0] - 1
+        self.seg_dic = {(0, 0, 0): (0, 'unlabeled')}
+        self.init()
+
+    def init(self):
+        pass
+
+    def data_frame_creator(self):
+        """Pandas dataFrame for addresses of images and corresponding labels"""
+
+        return pd.DataFrame()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self.next()
+
+    def next(self):
+        """Retrieve the next pairs from the dataset"""
+
+        if self.index - self.batch_size <= 0:
+            if not self.repeater:
+                raise StopIteration
+            else:
+                self.index = self.dataset.shape[0] - 1
+        self.index = self.index - self.batch_size
+
+        # loading features(images)
+        features = imread(self.dataset.loc[:, 'image'].iat[0])[:, :, :3]
+
+        if self.output_shape is None:
+            output_shape = features.shape[:2]
+        else:
+            output_shape = self.output_shape
+
+        # 1) Resize image to match a certain size.
+        # 2) Also the input image is converted (from 8-bit integer)
+        # to 64-bit floating point(->preserve_range=False).
+        # 3) [:, :, :3] -> to remove 4th channel in png
+        features = np.array([
+            resize(
+                image=imread(self.dataset.loc[:, 'image'].iat[i])[:, :, :3],
+                output_shape=output_shape,
+                mode='constant',
+                preserve_range=False,
+                anti_aliasing=True)
+            for i in range(self.index, self.index + self.batch_size)
+        ])
+
+        if self.data_type is 'float32':
+            features = img_as_float32(features)
+
+        # loading labels(segmentation)
+        if 'segmentation' in self.label_type:
+            # 1) Resize segmentation to match a certain size.
+            # 2) [:, :, :3] -> to remove 4th channel in png
+            segmentation = np.array([
+                imread(
+                    self.dataset.loc[:, 'segmentation'].iat[i])[:, :, :3]
+                    for i in range(self.index, self.index + self.batch_size)
+            ])
+
+            # resize(image=,
+            # output_shape=output_shape,
+            #         mode='constant',
+            #         preserve_range=True,
+            #         anti_aliasing=True)
+
+            # new_segmentation = np.zeros(
+            #     shape=(self.batch_size, output_shape[0], output_shape[1],
+            #            len(self.seg_dic)))
+            # for i in range(self.batch_size):
+            #     for j in range(output_shape[0]):
+            #         for k in range(output_shape[1]):
+            #             new_segmentation[i, j, k, 
+            #             self.seg_dic[ 
+            #                 tuple(segmentation[i, j, k]) ][0]] = 1
+            # segmentation = new_segmentation
+            
+            if self.data_type is 'float32':
+                segmentation = img_as_float32(segmentation)
+            else:
+                segmentation = img_as_float64(segmentation)
+
+        # if self.label_type == 'depth':
+        #     labels = np.array(
+        #         np.array([
+        #             resize(
+        #                 image=imread(self.dataset.iloc[i, 1]),
+        #                 output_shape=(480, 640))
+        #             for i in range(self.index, self.index + self.batch_size)
+        #         ]),
+        #         dtype=np.int32)
+        #     labels = (labels[:, :, :, 0] + labels[:, :, :, 1] * 256 +
+        #               labels[:, :, :, 2] * 256 * 256) / ((256 * 256 * 256) - 1)
+
+        # elif self.label_type == 'segmentation':
+        #     labels = np.array(
+        #         np.array([
+        #             resize(
+        #                 image=imread(self.dataset.iloc[i, 2])[:, :, 0],
+        #                 output_shape=(480, 640))
+        #             for i in range(self.index, self.index + self.batch_size)
+        #         ]))
+
+        #     new_segmentation = np.ndarray(shape=(self.batch_size, 480, 640, 22))
+        #     for i in range(self.batch_size):
+        #         for j in range(480):
+        #             for k in range(640):
+        #                 if labels[i, j, k] < 22:
+        #                     new_segmentation[i, j, k, int(labels[i, j, k])] = 1
+        #     labels = new_segmentation
+        #     labels = np.array(labels, dtype=np.float32)
+        # else:
+        #     raise ValueError('invalid label type')
+
+        # return features, labels
+        return features, segmentation
 
 
 class NYU:
@@ -46,6 +193,7 @@ class NYU:
         features = np.array(
             self.file.root.images[self.index:self.index + self.batch_size])
         features = np.transpose(features, [0, 3, 2, 1])
+        features = np.array(features / 255., dtype=np.float16)
 
         if self.label_type == 'depth':
             label_set = self.file.root.depths
@@ -61,14 +209,16 @@ class NYU:
             labels = np.transpose(labels, [0, 2, 1])
             labels = np.reshape(
                 labels, (labels.shape[0], labels.shape[1], labels.shape[2], 1))
-            new_labels = np.ndarray(shape=(self.batch_size, 480, 640, 21))
+            new_segmentation = np.ndarray(
+                shape=(self.batch_size, 480, 640, 21))
 
             for i in range(self.batch_size):
                 for j in range(480):
                     for k in range(640):
                         if labels[i, j, k] < 21:
-                            new_labels[i, j, k, int(labels[i, j, k])] = 1
-            labels = new_labels
+                            new_segmentation[i, j, k, int(labels[i, j, k])] = 1
+            labels = new_segmentation
+            labels = np.array(labels, dtype=np.float16)
         else:
             raise ValueError('invalid label type')
 
@@ -161,14 +311,19 @@ class SynthiaSf:
 
         features = np.array(
             np.array([
-                imageio.imread(self.dataset.iloc[i, 0])
+                resize(
+                    image=imread(self.dataset.iloc[i, 0])[:, :, :3],
+                    output_shape=(480, 640))
                 for i in range(self.index, self.index + self.batch_size)
             ]))
+        # features = np.array(features / 255., dtype=np.float32)
 
         if self.label_type == 'depth':
             labels = np.array(
                 np.array([
-                    imageio.imread(self.dataset.iloc[i, 1])
+                    resize(
+                        image=imread(self.dataset.iloc[i, 1]),
+                        output_shape=(480, 640))
                     for i in range(self.index, self.index + self.batch_size)
                 ]),
                 dtype=np.int32)
@@ -176,12 +331,86 @@ class SynthiaSf:
                       labels[:, :, :, 2] * 256 * 256) / ((256 * 256 * 256) - 1)
 
         elif self.label_type == 'segmentation':
-            labels = np.array(
-                np.array([
-                    imageio.imread(self.dataset.iloc[i, 2])[:, :, 0]
+            labels = np.array([
+                    resize(
+                        image=imread(self.dataset.iloc[i, 2])[:, :, 0],
+                        output_shape=(480, 640))
                     for i in range(self.index, self.index + self.batch_size)
-                ]))
+                ])
+            labels = img_as_ubyte(labels)
+
+            new_segmentation = np.ndarray(
+                shape=(self.batch_size, 480, 640, 22))
+            for i in range(self.batch_size):
+                for j in range(480):
+                    for k in range(640):
+                        if labels[i, j, k] < 22:
+                            new_segmentation[i, j, k, int(labels[i, j, k])] = 1
+            labels = new_segmentation
+
+        elif self.label_type == 'sparse_segmentation':
+            labels = np.array([
+                    resize(
+                        image=imread(self.dataset.iloc[i, 2])[:, :, 0],
+                        output_shape=(480, 640, 1))
+                    for i in range(self.index, self.index + self.batch_size)
+                ])
+            labels = img_as_ubyte(labels)
+
+            # new_segmentation = np.ndarray(
+            #     shape=(self.batch_size, 480, 640, 22))
+            # for i in range(self.batch_size):
+            #     for j in range(480):
+            #         for k in range(640):
+            #             if labels[i, j, k] < 22:
+            #                 new_segmentation[i, j, k, int(labels[i, j, k])] = 1
+            # labels = new_segmentation
+            # labels = np.array(labels, dtype=np.float32)
+            # labels = img_as_ubyte(labels)
         else:
             raise ValueError('invalid label type')
 
         return features, labels
+
+
+class VIPER(DatasetGenerator):
+    """Iterator for looping over elements of a VIPER(PlayForBenchmark) dataset ."""
+
+    def init(self):
+        self.seg_dic = segmentation_dics.VIPER
+
+    def data_frame_creator(self):
+        """Pandas dataFrame for addresses of images and corresponding labels"""
+        img_dir_list = [
+            self.dataset_dir + '/train' + '/img' + '/' + seq_dir + '/' +
+            img_name
+            for seq_dir in os.listdir(self.dataset_dir + '/train/' + '/img/')
+            for img_name in os.listdir(self.dataset_dir + '/train/' + '/img/' +
+                                       seq_dir)
+        ]
+
+        cls_dir_list = [
+            self.dataset_dir + '/train' + '/cls' + '/' + seq_dir + '/' +
+            img_name
+            for seq_dir in os.listdir(self.dataset_dir + '/train/' + '/cls/')
+            for img_name in os.listdir(self.dataset_dir + '/train/' + '/cls/' +
+                                       seq_dir)
+        ]
+
+        inst_dir_list = [
+            self.dataset_dir + '/train' + '/inst' + '/' + seq_dir + '/' +
+            img_name
+            for seq_dir in os.listdir(self.dataset_dir + '/train/' + '/inst/')
+            for img_name in os.listdir(self.dataset_dir + '/train/' +
+                                       '/inst/' + seq_dir)
+        ]
+
+        dataset = {
+            'image': img_dir_list,
+            'segmentation': cls_dir_list,
+            'instance': inst_dir_list
+        }
+
+        if self.shuffle:
+            return pd.DataFrame(dataset).sample(frac=1)
+        return pd.DataFrame(dataset)
