512fc7a5ec18e84acb16bad4e4d5233250529cea
Amin <m.amin.kashi@gmail.com>
2019-05-22 03:12:31 +0430

    Update model.py

--- model.py
+++ model.py
@@ -7,11 +7,24 @@ import deep_lab_decoder
 
 MODEL = tf.keras.Model
 
+
 def deep_lab_v3(inputs):
     """Implementation of the Deeplabv3+ """
-
-    result, skip = xception.xception_71(inputs=inputs)
-    result = aspp.aspp(inputs=result, input_shape=[480, 640, 3])
-    result = deep_lab_decoder.decoder(inputs=result, skip=skip)
+    batch_normalization = True
+    weight_decay = 0.05
+    result, skip = xception.xception_71(
+        inputs=inputs,
+        weight_decay=weight_decay,
+        batch_normalization=batch_normalization)
+    result = aspp.aspp(
+        inputs=result,
+        input_shape=[480, 640, 3],
+        weight_decay=weight_decay,
+        batch_normalization=batch_normalization)
+    result = deep_lab_decoder.decoder(
+        inputs=result,
+        skip=skip,
+        weight_decay=weight_decay,
+        batch_normalization=batch_normalization)
     model = MODEL(inputs=inputs, outputs=result, name='Deeplabv3+')
     return model
