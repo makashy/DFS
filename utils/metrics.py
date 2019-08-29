c38d0c74a015a6f5f9d0929e518f8fad52255675
Amin <m.amin.kashi@gmail.com>
2019-08-29 18:13:44 +0430

    Create metrics.py

---
+++ utils/metrics.py
@@ -0,0 +1,88 @@
+"""Contains metrics for evaluating segmentation DNN """
+import numpy as np
+from numba import jit
+import tensorflow as tf
+
+
+def mean_IOU(y_true, y_pred):
+    "Calculates mean intersection over uninon over all classes"
+    smooth = 1.
+    y_true_f = tf.keras.backend.reshape(y_true, [-1])
+    y_pred_f = tf.keras.backend.reshape(y_pred, [-1])
+    true_positive = tf.keras.backend.sum(y_true_f * y_pred_f)
+    score = (true_positive + smooth) / (
+        tf.keras.backend.sum(y_true_f) + tf.keras.backend.sum(y_pred_f) -
+        true_positive)  # = TP/(TP + FP + FN)
+    return score
+
+
+def mean_IOU_loss(y_true, y_pred):
+    "Calculates IOU loss"
+    loss = 1 - mean_IOU(y_true, y_pred)
+    return loss
+############################################################tensorflow : bce_dice_loss
+def dice_coeff(y_true, y_pred):
+    ""
+    smooth = 1.
+    y_true_f = tf.reshape(y_true, [-1])
+    y_pred_f = tf.reshape(y_pred, [-1])
+    intersection = tf.reduce_sum(y_true_f * y_pred_f)
+    score = (2. * intersection + smooth) / (tf.reduce_sum(y_true_f) +
+                                            tf.reduce_sum(y_pred_f) + smooth)
+    return score
+
+
+def dice_loss(y_true, y_pred):
+    loss = 1 - dice_coeff(y_true, y_pred)
+    return loss
+
+
+def bce_dice_loss(y_true, y_pred):
+    loss = tf.keras.losses.categorical_crossentropy(
+        y_true, y_pred) + dice_loss(y_true, y_pred)
+    return loss
+############################################################numpy : bce_dice_loss
+def dice_coeff_np(y_true, y_pred):
+    ""
+    smooth = 1.
+    y_true_f = np.reshape(y_true, [-1])
+    y_pred_f = np.reshape(y_pred, [-1])
+    intersection = np.sum(y_true_f * y_pred_f)
+    score = (2. * intersection + smooth) / (np.sum(y_true_f) +
+                                            np.sum(y_pred_f) + smooth)
+    return score
+
+
+def dice_loss_np(y_true, y_pred):
+    loss = 1 - dice_coeff_np(y_true, y_pred)
+    return loss
+
+
+def bce_dice_loss_np(y_true, y_pred):
+    loss =  tf.cast(tf.numpy_function(dice_loss_np, [y_true, y_pred], tf.float64), tf.float32) + \
+    tf.keras.losses.categorical_crossentropy(y_true, y_pred)
+    return loss
+############################################################numba : bce_dice_loss
+@jit
+def dice_coeff_nb(y_true, y_pred):
+    ""
+    smooth = np.float32(1)
+    y_true_f = np.reshape(y_true, [-1])
+    y_pred_f = np.reshape(y_pred, [-1])
+    intersection = np.sum(y_true_f * y_pred_f)
+    score = (2. * intersection + smooth) / (np.sum(y_true_f) +
+                                            np.sum(y_pred_f) + smooth)
+    return score
+
+@jit
+def dice_loss_nb(y_true, y_pred):
+    loss = 1 - dice_coeff_nb(y_true, y_pred)
+    return loss
+
+
+def bce_dice_loss_nb(y_true, y_pred):
+    loss =  tf.cast(tf.numpy_function(dice_loss_nb, [y_true, y_pred], tf.float64), tf.float32) + \
+    tf.keras.losses.categorical_crossentropy(y_true, y_pred)
+    return loss
+
+##############################################################################
