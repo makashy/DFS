539009dc852cf4f8dd6fe02d815ce031d45a67d3
Amin Kashi <m.amin.kashi@gmail.com>
2019-12-24 11:28:43 +0330

    clean up

--- utils/fcrn.py
+++ utils/fcrn.py
@@ -13,7 +13,11 @@ from tensorflow.keras.layers import (  # pylint: disable=import-error
 ACTIVATION = 'relu'  #'selu'
 
 
-def baseline(input_tensor, input_filter, output_filter, block_name, batch_normalization=True):
+def baseline(input_tensor,
+             input_filter,
+             output_filter,
+             block_name,
+             batch_normalization=True):
     "Baseline block"
     with tf.name_scope(block_name):
         result = Conv2D(filters=input_filter, kernel_size=1,
@@ -38,7 +42,12 @@ def baseline(input_tensor, input_filter, output_filter, block_name, batch_normal
     return result
 
 
-def bottleneck(input_tensor, input_filter, output_filter, strides, block_name, batch_normalization=True):
+def bottleneck(input_tensor,
+               input_filter,
+               output_filter,
+               strides,
+               block_name,
+               batch_normalization=True):
     "Bottleneck block"
     with tf.name_scope(block_name):
         result = Conv2D(filters=input_filter, kernel_size=1,
@@ -86,33 +95,37 @@ def interleave(tensors, axis):
                                     new_shape)
 
 
-def unpool_as_conv(input_tensor, output_filter, activation, name, batch_normalization=True):
+def unpool_as_conv(input_tensor,
+                   output_filter,
+                   activation,
+                   name,
+                   batch_normalization=True):
     "Figure 3 in paper[1]↑"
     with tf.name_scope(name):
-        result_A = Conv2D(
-            filters=output_filter,
-            kernel_size=(3, 3),
-            strides=1,
-            padding='SAME',
-            name=tf.compat.v1.get_default_graph().unique_name('result_A'))(input_tensor)
-        result_B = Conv2D(
-            filters=output_filter,
-            kernel_size=(2, 3),
-            strides=1,
-            padding='SAME',
-            name=tf.compat.v1.get_default_graph().unique_name('result_B'))(input_tensor)
-        result_C = Conv2D(
-            filters=output_filter,
-            kernel_size=(3, 2),
-            strides=1,
-            padding='SAME',
-            name=tf.compat.v1.get_default_graph().unique_name('result_C'))(input_tensor)
-        result_D = Conv2D(
-            filters=output_filter,
-            kernel_size=(2, 2),
-            strides=1,
-            padding='SAME',
-            name=tf.compat.v1.get_default_graph().unique_name('result_D'))(input_tensor)
+        result_A = Conv2D(filters=output_filter,
+                          kernel_size=(3, 3),
+                          strides=1,
+                          padding='SAME',
+                          name=tf.compat.v1.get_default_graph().unique_name(
+                              'result_A'))(input_tensor)
+        result_B = Conv2D(filters=output_filter,
+                          kernel_size=(2, 3),
+                          strides=1,
+                          padding='SAME',
+                          name=tf.compat.v1.get_default_graph().unique_name(
+                              'result_B'))(input_tensor)
+        result_C = Conv2D(filters=output_filter,
+                          kernel_size=(3, 2),
+                          strides=1,
+                          padding='SAME',
+                          name=tf.compat.v1.get_default_graph().unique_name(
+                              'result_C'))(input_tensor)
+        result_D = Conv2D(filters=output_filter,
+                          kernel_size=(2, 2),
+                          strides=1,
+                          padding='SAME',
+                          name=tf.compat.v1.get_default_graph().unique_name(
+                              'result_D'))(input_tensor)
 
         # Interleaving elements of the four feature maps
         # --------------------------------------------------
@@ -128,7 +141,10 @@ def unpool_as_conv(input_tensor, output_filter, activation, name, batch_normaliz
     return result
 
 
-def up_project(input_tensor, output_filter, block_name, batch_normalization=True):
+def up_project(input_tensor,
+               output_filter,
+               block_name,
+               batch_normalization=True):
     "Figure 2, (d) in paper[1]↑"
     with tf.name_scope(block_name):
         result_1 = unpool_as_conv(input_tensor=input_tensor,
@@ -154,6 +170,7 @@ def up_project(input_tensor, output_filter, block_name, batch_normalization=True
         result = Activation(ACTIVATION)(result)
 
     return result
+
 
 ## M0: original FCRN (With addition of a up_project at last layers
 # and additional con2d at final layer)
@@ -271,14 +288,17 @@ def model_m0(img_shape=(480, 640, 3)):
                         output_filter=32,
                         block_name='up_project_additional')
 
-
     # result = tf.layers.dropout(inputs=result, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
-    outputs = Conv2D(filters=1, kernel_size=1, strides=1, name='predict')(result)
+    outputs = Conv2D(filters=1, kernel_size=1, strides=1,
+                     name='predict')(result)
 
     return tf.keras.models.Model(inputs=inputs, outputs=outputs, name='FCRN')
 
+
 ## M1: It only use a concatenated input with combination of RGB image and Semantic Segmentation Map
-def model_m1(rgb_shape=(480, 640, 3), seg_shape=(480, 640, 19), batch_normalization=True):
+def model_m1(rgb_shape=(480, 640, 3),
+             seg_shape=(480, 640, 19),
+             batch_normalization=True):
     """FCRN model"""
 
     inputs_rgb = Input(shape=rgb_shape)
@@ -303,13 +323,13 @@ def model_m1(rgb_shape=(480, 640, 3), seg_shape=(480, 640, 19), batch_normalizat
                       input_filter=64,
                       output_filter=256,
                       block_name='baseline',
-                        batch_normalization=batch_normalization)
+                      batch_normalization=batch_normalization)
 
     result = baseline(input_tensor=result,
                       input_filter=64,
                       output_filter=256,
                       block_name='baseline',
-                        batch_normalization=batch_normalization)
+                      batch_normalization=batch_normalization)
 
     result = bottleneck(input_tensor=result,
                         input_filter=128,
@@ -322,19 +342,19 @@ def model_m1(rgb_shape=(480, 640, 3), seg_shape=(480, 640, 19), batch_normalizat
                       input_filter=128,
                       output_filter=512,
                       block_name='baseline',
-                        batch_normalization=batch_normalization)
+                      batch_normalization=batch_normalization)
 
     result = baseline(input_tensor=result,
                       input_filter=128,
                       output_filter=512,
                       block_name='baseline',
-                        batch_normalization=batch_normalization)
+                      batch_normalization=batch_normalization)
 
     result = baseline(input_tensor=result,
                       input_filter=128,
                       output_filter=512,
                       block_name='baseline',
-                        batch_normalization=batch_normalization)
+                      batch_normalization=batch_normalization)
 
     result = bottleneck(input_tensor=result,
                         input_filter=256,
@@ -425,6 +445,7 @@ def model_m1(rgb_shape=(480, 640, 3), seg_shape=(480, 640, 19), batch_normalizat
     return tf.keras.models.Model(inputs=[inputs_rgb, inputs_seg],
                                  outputs=outputs,
                                  name='FCRN')
+
 
 ## M2: It has additional decoder branch for each semantic class
 def model_m2(rgb_shape=(480, 640, 3), seg_shape=(480, 640, 19)):
@@ -542,7 +563,8 @@ def model_m2(rgb_shape=(480, 640, 3), seg_shape=(480, 640, 19)):
         temp = Conv2D(filters=4,
                       kernel_size=3,
                       padding='same',
-                      name=tf.compat.v1.get_default_graph().unique_name('seperator'))(result)
+                      name=tf.compat.v1.get_default_graph().unique_name(
+                          'seperator'))(result)
         temp = up_project(input_tensor=temp,
                           output_filter=2,
                           block_name='up_project')
@@ -552,11 +574,13 @@ def model_m2(rgb_shape=(480, 640, 3), seg_shape=(480, 640, 19)):
         semantic_depth.append(temp)
 
     complete_depth_map = Add(name='complete_depth_map')(semantic_depth)
-    semantic_depth_maps = Concatenate(name='semantic_depth_maps')(semantic_depth)
+    semantic_depth_maps = Concatenate(
+        name='semantic_depth_maps')(semantic_depth)
 
-    return tf.keras.models.Model(inputs=[inputs_rgb, inputs_seg],
-                                 outputs=[complete_depth_map, semantic_depth_maps],
-                                 name='FCRN')
+    return tf.keras.models.Model(
+        inputs=[inputs_rgb, inputs_seg],
+        outputs=[complete_depth_map, semantic_depth_maps],
+        name='FCRN')
 
 
 ## M3: It has additional decoder branch for each semantic class
@@ -675,7 +699,8 @@ def model_m3(rgb_shape=(480, 640, 3), seg_shape=(480, 640, 19)):
         temp = Conv2D(filters=4,
                       kernel_size=3,
                       padding='same',
-                      name=tf.compat.v1.get_default_graph().unique_name('seperator'))(result)
+                      name=tf.compat.v1.get_default_graph().unique_name(
+                          'seperator'))(result)
         temp = up_project(input_tensor=temp,
                           output_filter=2,
                           block_name='up_project')
@@ -818,12 +843,14 @@ def model_m4(rgb_shape=(480, 640, 3), seg_shape=(480, 640, 19)):
     for i in range(seg_shape[2]):
         with tf.name_scope('semantic_depth_layer_' + str(i)):
             print('semantic_depth_layer_' + str(i))
-            sem_rgb = tf.multiply(inputs_rgb, tf.expand_dims(inputs_seg[:, :, :, i], -1))
+            sem_rgb = tf.multiply(inputs_rgb,
+                                  tf.expand_dims(inputs_seg[:, :, :, i], -1))
 
             temp = Conv2D(filters=32,
                           kernel_size=3,
                           padding='same',
-                          name=tf.compat.v1.get_default_graph().unique_name('seperator'))(result)
+                          name=tf.compat.v1.get_default_graph().unique_name(
+                              'seperator'))(result)
 
             temp = up_project(input_tensor=temp,
                               output_filter=16,
@@ -971,7 +998,9 @@ def model_m5(rgb_shape=(480, 640, 3), seg_shape=(480, 640, 19)):
         with tf.name_scope('semantic_depth_layer_' + str(i)):
             print('semantic_depth_layer_' + str(i))
 
-            sem_rgb = Multiply()([inputs_rgb, tf.expand_dims(inputs_seg[:, :, :, i], -1)])
+            sem_rgb = Multiply()(
+                [inputs_rgb,
+                 tf.expand_dims(inputs_seg[:, :, :, i], -1)])
 
             temp = Concatenate()([result, tf.image.resize(sem_rgb, (60, 80))])
 
@@ -988,8 +1017,11 @@ def model_m5(rgb_shape=(480, 640, 3), seg_shape=(480, 640, 19)):
                               output_filter=1,
                               block_name='up_project')
 
-            temp = Multiply()(
-                [temp, tf.image.resize(tf.expand_dims(inputs_seg[:, :, :, i], -1), (480, 640))])
+            temp = Multiply()([
+                temp,
+                tf.image.resize(tf.expand_dims(inputs_seg[:, :, :, i], -1),
+                                (480, 640))
+            ])
             semantic_depth.append(temp)
 
     predict = Add()(semantic_depth)
