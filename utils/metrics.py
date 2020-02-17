416c9229b8369206853cf003fd84c669d661d42a
Amin Kashi <m.amin.kashi@gmail.com>
2020-02-17 16:50:36 +0330

    Depth metrics compatibility for LIDAR
    Depth metrics compatibility for LIDAR:
    
    Acceptable error calculation area (aeca): prevent calculation of error in distances less than 'minimum_y_true'. This also prevents the calculation of error where LIDAR data is not available.

--- utils/metrics.py
+++ utils/metrics.py
@@ -145,11 +145,15 @@ class MAPE(tf.keras.losses.Loss):
         self.minimum_y_true = minimum_y_true
 
     def call(self, y_true, y_pred):
-        "Computes and returns the metric value tensor."
+        """Computes and returns the metric value tensor.
+
+            Acceptable error calculation area (aeca): prevent calculation of error in distances less
+            than 'minimum_y_true'. This also prevents the calculation of error where LIDAR data is
+            not available.
+        """
+        aeca = tf.cast(y_true > self.minimum_y_true, tf.float32)
         return tf.reduce_mean(
-            tf.abs(y_true - y_pred) *
-            tf.cast(tf.logical_not(y_true < self.minimum_y_true), tf.float32) /
-            (y_true + self.smooth) * 100)
+            tf.abs(y_true - y_pred) / (y_true + self.smooth) * 100 * aeca)
 
 
 class MSPE(tf.keras.losses.Loss):
@@ -173,11 +177,15 @@ class MSPE(tf.keras.losses.Loss):
         self.minimum_y_true = minimum_y_true
 
     def call(self, y_true, y_pred):
-        "Computes and returns the metric value tensor."
+        """Computes and returns the metric value tensor.
+
+            Acceptable error calculation area (aeca): prevent calculation of error in distances less
+            than 'minimum_y_true'. This also prevents the calculation of error where LIDAR data is
+            not available.
+        """
+        aeca = tf.cast(y_true > self.minimum_y_true, tf.float32)
         return tf.reduce_mean(
-            tf.pow(y_true - y_pred, 2) *
-            tf.cast(tf.logical_not(y_true < self.minimum_y_true), tf.float32) /
-            (y_true + self.smooth) * 100)
+            tf.pow(y_true - y_pred, 2) / (y_true + self.smooth) * 100 * aeca)
 
 
 class RMSE(tf.keras.losses.Loss):
@@ -185,43 +193,72 @@ class RMSE(tf.keras.losses.Loss):
     Alternatives:
         tf.keras.metrics.RootMeanSquaredError
     """
-    def __init__(self, reduction=tf.keras.losses.Reduction.AUTO):
+    def __init__(self,
+                 minimum_y_true=0.5,
+                 reduction=tf.keras.losses.Reduction.AUTO):
         super().__init__(reduction=reduction, name='RMSE')
+        self.minimum_y_true = minimum_y_true
 
     def call(self, y_true, y_pred):
-        "Computes and returns the metric value tensor."
-        return tf.sqrt(tf.reduce_mean(tf.pow(y_true - y_pred, 2)))
+        """Computes and returns the metric value tensor.
+
+            Acceptable error calculation area (aeca): prevent calculation of error in distances less
+            than 'minimum_y_true'. This also prevents the calculation of error where LIDAR data is
+            not available.
+        """
+        aeca = tf.cast(y_true > self.minimum_y_true, tf.float32)
+        return tf.sqrt(tf.reduce_mean(tf.pow(y_true - y_pred, 2) * aeca))
 
 
 class RMSELog(tf.keras.losses.Loss):
     """Computes root mean squared error metric in log space  between y_true and y_pred.
     """
-    def __init__(self, smooth=1e-6, reduction=tf.keras.losses.Reduction.AUTO):
+    def __init__(self,
+                 smooth=1e-6,
+                 minimum_y_true=0.5,
+                 reduction=tf.keras.losses.Reduction.AUTO):
         super().__init__(reduction=reduction, name='RMSELog')
         self.smooth = smooth
+        self.minimum_y_true = minimum_y_true
 
     def call(self, y_true, y_pred):
-        "Computes and returns the metric value tensor."
+        """Computes and returns the metric value tensor.
+
+            Acceptable error calculation area (aeca): prevent calculation of error in distances less
+            than 'minimum_y_true'. This also prevents the calculation of error where LIDAR data is
+            not available.
+        """
+        aeca = tf.cast(y_true > self.minimum_y_true, tf.float32)
         return tf.sqrt(
             tf.reduce_mean(
                 tf.pow(
                     tf.math.log(y_true + self.smooth) -
-                    tf.math.log(y_pred + self.smooth), 2)))
+                    tf.math.log(y_pred + self.smooth), 2) * aeca))
 
 
 class Log10(tf.keras.losses.Loss):
     """Computes mean absolute error metric in log10 space between y_true and y_pred.
     """
-    def __init__(self, smooth=1e-6, reduction=tf.keras.losses.Reduction.AUTO):
+    def __init__(self,
+                 smooth=1e-6,
+                 minimum_y_true=0.5,
+                 reduction=tf.keras.losses.Reduction.AUTO):
         super().__init__(reduction=reduction, name='Log10')
         self.smooth = smooth
+        self.minimum_y_true = minimum_y_true
 
     def call(self, y_true, y_pred):
-        "Computes and returns the metric value tensor."
+        """Computes and returns the metric value tensor.
+
+            Acceptable error calculation area (aeca): prevent calculation of error in distances less
+            than 'minimum_y_true'. This also prevents the calculation of error where LIDAR data is
+            not available.
+        """
+        aeca = tf.cast(y_true > self.minimum_y_true, tf.float32)
         return tf.reduce_mean(
             tf.abs(
                 tf.math.log(y_true + self.smooth) -
-                tf.math.log(y_pred + self.smooth)))
+                tf.math.log(y_pred + self.smooth)) * aeca)
 
 
 class DeltaThreshold(tf.keras.losses.Loss):
@@ -229,30 +266,51 @@ class DeltaThreshold(tf.keras.losses.Loss):
     """
     def __init__(self,
                  smooth=1e-6,
+                 minimum_y_true=0.5,
                  i=1,
+                 threshold=1.25,
                  reduction=tf.keras.losses.Reduction.AUTO):
         super().__init__(reduction=reduction, name='delta_threshold_' + str(i))
-        self.i = i
         self.smooth = smooth
+        self.minimum_y_true = minimum_y_true
+        self.i = i
+        self.threshold = threshold
 
     def call(self, y_true, y_pred):
-        "Computes and returns the metric value tensor."
+        """Computes and returns the metric value tensor.
+
+            Acceptable error calculation area (aeca): prevent calculation of error in distances less
+            than 'minimum_y_true'. This also prevents the calculation of error where LIDAR data is
+            not available.
+        """
+        aeca = tf.cast(y_true > self.minimum_y_true, tf.float32)
         return tf.math.count_nonzero(
-            tf.maximum(y_true / (self.smooth + y_pred), y_pred /
-                       (self.smooth + y_true)) < 1.25**self.i) / tf.size(
-                           y_true, out_type=tf.dtypes.int64)
+            (tf.maximum(y_true / (self.smooth + y_pred), y_pred /
+                        (self.smooth + y_true)) < self.threshold**self.i) *
+            aeca) / tf.math.count_nonzero(y_true * aeca)
 
 
 class SILog(tf.keras.losses.Loss):
     """Computes  Scale invariant logarithmic error metric in between y_true and y_pred.
     """
-    def __init__(self, smooth=1e-6, reduction=tf.keras.losses.Reduction.AUTO):
+    def __init__(self,
+                 smooth=1e-6,
+                 minimum_y_true=0.5,
+                 reduction=tf.keras.losses.Reduction.AUTO):
         super().__init__(reduction=reduction, name='SILog')
         self.smooth = smooth
+        self.minimum_y_true = minimum_y_true
 
     def call(self, y_true, y_pred):
-        "Computes and returns the metric value tensor."
-        difference = tf.math.log(y_true + self.smooth) - \
-                     tf.math.log(y_pred + self.smooth)
+        """Computes and returns the metric value tensor.
+
+            Acceptable error calculation area (aeca): prevent calculation of error in distances less
+            than 'minimum_y_true'. This also prevents the calculation of error where LIDAR data is
+            not available.
+        """
+
+        aeca = tf.cast(y_true > self.minimum_y_true, tf.float32)
+        difference = (tf.math.log(y_true + self.smooth) - \
+                     tf.math.log(y_pred + self.smooth)) * aeca
         return tf.reduce_mean(tf.pow(difference, 2)) - tf.pow(
             tf.reduce_mean(difference), 2)
